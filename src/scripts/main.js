eef9a8aea3f7afb9ea0845a78cecb6f90f03d5bf
Kasiei <k25ts09@outlook.jp>
2025-09-25 12:53:40 +0900

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -17,192 +17,230 @@ const mainScript = () => {
 
   const footer = document.querySelector(".footer");
 
-  // if (window.location.pathname === "/") {
-  //   const html = document.querySelector("html");
-  //   const body = document.querySelector("body");
-  //   const main = document.querySelector(".main");
-  //   const loading = document.querySelector(".js-loading");
-  //   const kvTagText = document.querySelectorAll(".p-top-kv__tag-line");
-  //   // gsap.set([html], {
-  //   //   overflow: "hidden",
-  //   //   height: "100%",
-  //   // });
-  //   gsap.set(loading, {
-  //     display: "block",
-  //     opacity: 1,
-  //     pointerEvents: "auto",
-  //   });
-  //   // gsap.set(main, {
-  //   //   pointerEvents: "none",
-  //   // });
-  //   gsap.set(kvTagText, {
-  //     yPercent: 100,
-  //   });
+  if (window.location.pathname === "/") {
+    const html = document.querySelector("html");
+    const body = document.querySelector("body");
+    const main = document.querySelector(".main");
+    const loading = document.querySelector(".js-loading");
+    const kvTagText = document.querySelectorAll(".p-top-kv__tag-line");
+    // gsap.set([html], {
+    //   overflow: "hidden",
+    //   height: "100%",
+    // });
+    gsap.set(loading, {
+      display: "block",
+      opacity: 1,
+      pointerEvents: "auto",
+    });
+    // gsap.set(main, {
+    //   pointerEvents: "none",
+    // });
+    gsap.set(kvTagText, {
+      yPercent: 100,
+    });
 
-  //   const digitWrappers = document.querySelectorAll(".js-digit");
-  //   const digitHeight = digitWrappers[0].getBoundingClientRect().height;
-  //   const loadingText = document.querySelectorAll(".loading__text");
-  //   digitWrappers.forEach((item) => {
-  //     item.style.height = digitHeight + "px";
-  //   });
+    const digitWrappers = document.querySelectorAll(".js-digit");
+    const digitHeight = digitWrappers[0].getBoundingClientRect().height;
+    const loadingText = document.querySelectorAll(".loading__text");
+    digitWrappers.forEach((item) => {
+      item.style.height = digitHeight + "px";
+    });
 
-  //   digitWrappers.forEach((wrapper) => {
-  //     for (let i = 1; i <= 9; i++) {
-  //       const span = document.createElement("span");
-  //       span.classList.add("digit");
-  //       span.classList.add("loading__text");
-  //       span.textContent = i;
-  //       wrapper.appendChild(span);
-  //     }
+    digitWrappers.forEach((wrapper) => {
+      for (let i = 1; i <= 9; i++) {
+        const span = document.createElement("span");
+        span.classList.add("digit");
+        span.classList.add("loading__text");
+        span.textContent = i;
+        wrapper.appendChild(span);
+      }
 
-  //     digitWrappers[1].querySelectorAll(".digit").forEach((digit) => {
-  //       digit.classList.add("digit-one");
-  //     });
+      digitWrappers[1].querySelectorAll(".digit").forEach((digit) => {
+        digit.classList.add("digit-one");
+      });
 
-  //     digitWrappers[0].querySelectorAll(".digit").forEach((digit) => {
-  //       digit.classList.add("digit-ten");
-  //     });
-  //   });
+      digitWrappers[0].querySelectorAll(".digit").forEach((digit) => {
+        digit.classList.add("digit-ten");
+      });
+    });
 
-  //   const digits = document.querySelectorAll(".digit");
-  //   const digitone = document.querySelectorAll(".digit-one");
-  //   const digitten = document.querySelectorAll(".digit-ten");
-  //   const digithundred = document.querySelector(".digit-hundred");
+    const digits = document.querySelectorAll(".digit");
+    const digitone = document.querySelectorAll(".digit-one");
+    const digitten = document.querySelectorAll(".digit-ten");
+    const digithundred = document.querySelector(".digit-hundred");
 
-  //   const openingAnimation = () => {
-  //     const timeline = gsap.timeline();
-  //     const digitoneFirst = Math.floor(Math.random() * 9) + 1;
-  //     const digittenFirst = Math.floor(Math.random() * 8) + 1;
-  //     const spinDuration = 1.5;
-  //     const easing = "power1.out";
+    const openingAnimation = () => {
+      const timeline = gsap.timeline();
+      const digitoneFirst = Math.floor(Math.random() * 9) + 1;
+      const digittenFirst = Math.floor(Math.random() * 8) + 1;
+      const spinDuration = 1.5;
+      const easing = "power1.out";
 
-  //     gsap.set(digithundred, {
-  //       yPercent: 100,
-  //       display: "block",
-  //     });
+      gsap.set(digithundred, {
+        yPercent: 100,
+        display: "block",
+      });
 
-  //     timeline.to(
-  //       digitone,
-  //       {
-  //         y: -digitHeight * digitoneFirst,
-  //         duration: spinDuration,
-  //         ease: easing,
-  //       },
-  //       "start"
-  //     );
+      timeline.to(
+        digitone,
+        {
+          y: -digitHeight * digitoneFirst,
+          duration: spinDuration,
+          ease: easing,
+        },
+        "start"
+      );
 
-  //     timeline.to(
-  //       digitten,
-  //       {
-  //         y: -digitHeight * digittenFirst,
-  //         duration: spinDuration,
-  //         ease: easing,
-  //       },
-  //       "start"
-  //     );
+      timeline.to(
+        digitten,
+        {
+          y: -digitHeight * digittenFirst,
+          duration: spinDuration,
+          ease: easing,
+        },
+        "start"
+      );
 
-  //     const digitoneSecond = Math.floor(Math.random() * 9) + 1;
-  //     let digittenSecond =
-  //       Math.floor(Math.random() * (9 - digittenFirst)) + digittenFirst + 1;
-  //     if (digittenSecond > 9) digittenSecond === 9;
+      const digitoneSecond = Math.floor(Math.random() * 9) + 1;
+      let digittenSecond =
+        Math.floor(Math.random() * (9 - digittenFirst)) + digittenFirst + 1;
+      if (digittenSecond > 9) digittenSecond === 9;
 
-  //     timeline.to(
-  //       digitone,
-  //       {
-  //         y: -digitHeight * digitoneSecond,
-  //         duration: spinDuration,
-  //         ease: easing,
-  //       },
-  //       "second"
-  //     );
+      timeline.to(
+        digitone,
+        {
+          y: -digitHeight * digitoneSecond,
+          duration: spinDuration,
+          ease: easing,
+        },
+        "second"
+      );
 
-  //     timeline.to(
-  //       digitten,
-  //       {
-  //         y: -digitHeight * digittenSecond,
-  //         duration: spinDuration,
-  //         ease: easing,
-  //       },
-  //       "second"
-  //     );
+      timeline.to(
+        digitten,
+        {
+          y: -digitHeight * digittenSecond,
+          duration: spinDuration,
+          ease: easing,
+        },
+        "second"
+      );
 
-  //     timeline.to(
-  //       digits,
-  //       {
-  //         y: 0,
-  //         duration: spinDuration,
-  //         ease: easing,
-  //       },
-  //       "final"
-  //     );
+      timeline.to(
+        digits,
+        {
+          y: 0,
+          duration: spinDuration,
+          ease: easing,
+        },
+        "final"
+      );
 
-  //     timeline.to(
-  //       digithundred,
-  //       {
-  //         yPercent: 0,
-  //         duration: spinDuration,
-  //         ease: easing,
-  //       },
-  //       "final"
-  //     );
+      timeline.to(
+        digithundred,
+        {
+          yPercent: 0,
+          duration: spinDuration,
+          ease: easing,
+        },
+        "final"
+      );
 
-  //     timeline.to(loadingText, {
-  //       clipPath: "inset(0% 0% 100% 0%)",
-  //       duration: 1.5,
-  //       ease: "power2.out",
-  //     });
+      timeline.to(loadingText, {
+        clipPath: "inset(0% 0% 100% 0%)",
+        duration: 1.5,
+        ease: "power2.out",
+      });
 
-  //     timeline.to(
-  //       loading,
-  //       {
-  //         opacity: 0,
-  //         pointerEvents: "none",
-  //         duration: 2,
-  //         ease: "sine.in",
-  //         onComplete: () => {
-  //           gsap.set(loading, {
-  //             display: "none",
-  //             opacity: 0,
-  //             pointerEvents: "none",
-  //           });
-  //         },
-  //       },
-  //       "view"
-  //     );
+      timeline.to(
+        loading,
+        {
+          opacity: 0,
+          pointerEvents: "none",
+          duration: 2,
+          ease: "sine.in",
+          onComplete: () => {
+            gsap.set(loading, {
+              display: "none",
+              opacity: 0,
+              pointerEvents: "none",
+            });
+          },
+        },
+        "view"
+      );
 
-  //     timeline.to(
-  //       main,
-  //       {
-  //         pointerEvents: "auto",
-  //         duration: 1,
-  //         ease: "sine.in",
-  //       },
-  //       "view"
-  //     );
+      timeline.to(
+        main,
+        {
+          pointerEvents: "auto",
+          duration: 1,
+          ease: "sine.in",
+        },
+        "view"
+      );
 
-  //     timeline.to(
-  //       kvTagText,
-  //       {
-  //         yPercent: 0,
-  //         duration: 1.5,
-  //         ease: "power2.out",
-  //         onComplete: () => {
-  //           gsap.set([html], {
-  //             overflow: "auto",
-  //             height: "auto",
-  //           });
-  //           gsap.set(main, {
-  //             pointerEvents: "auto",
-  //           });
-  //           runLenis();
-  //         },
-  //       },
-  //       "+=0.5"
-  //     );
-  //   };
+      timeline.to(
+        kvTagText,
+        {
+          yPercent: 0,
+          duration: 1.5,
+          ease: "power2.out",
+          onComplete: () => {
+            gsap.set([html], {
+              overflow: "auto",
+              height: "auto",
+            });
+            gsap.set(main, {
+              pointerEvents: "auto",
+            });
+            runLenis();
+          },
+        },
+        "+=0.5"
+      );
+    };
 
-  //   openingAnimation();
-  // }
+    openingAnimation();
+
+    const cta = document.querySelector(".c-cta");
+    const ctaMask = document.querySelector(".js-cta-mask");
+    gsap.set(ctaMask, {
+      rotate: 0,
+      opacity: 1,
+      yPercent: 0,
+    });
+    gsap.to(ctaMask, {
+      rotate: 3,
+      opacity: 1,
+      yPercent: 0,
+      duration: 1.5,
+      ease: "sine.inOut",
+      repeat: -1,
+      yoyo: true,
+    });
+    gsap.to(ctaMask, {
+      scrollTrigger: {
+        trigger: cta,
+        start: "top+=100",
+        end: "bottom top",
+        markers: true,
+        onEnter: () => {
+          gsap.to(ctaMask, {
+            yPercent: 100,
+            opacity: 0,
+            ease: "sine",
+            duration: 2,
+            onComplete: () => {
+              gsap.set(ctaMask, {
+                display: "none",
+              });
+            },
+          });
+        },
+      },
+    });
+  }
 
   if (window.location.pathname === "/info") {
     runLenis();
@@ -220,6 +258,22 @@ const mainScript = () => {
         trigger: infoArchive,
         start: "top 80%",
         end: "bottom 10%",
+        scrub: true,
+      },
+    });
+
+    const cta = document.querySelector(".js-cta");
+    const ctaImage = document.querySelector(".js-cta-image");
+    gsap.set(ctaImage, {
+      backgroundPosition: "center 20%",
+    });
+
+    gsap.to(ctaImage, {
+      backgroundPosition: "center 40%",
+      scrollTrigger: {
+        trigger: cta,
+        start: "top bottom",
+        end: "bottom top",
         scrub: true,
       },
     });
@@ -321,7 +375,8 @@ const mainScript = () => {
   const hoverText = document.querySelectorAll(".js-hover-reveal");
   const defaultIcon = document.querySelectorAll(".js-hover-icon-default");
   const hoverIcon = document.querySelectorAll(".js-hover-icon-reveal");
-  console.log(hoverText);
+  const lg = window.matchMedia("(min-width: 1024px)");
+  let isHoverRegistered = false;
   gsap.set(defaultText, {
     yPercent: 0,
     opacity: 1,
@@ -338,30 +393,41 @@ const mainScript = () => {
     yPercent: 100,
     opacity: 0,
   });
-  hoverToggle.forEach((toggle, index) => {
-    toggle.addEventListener("mouseenter", () => {
-      gsap.set(defaultText[index], {
-        yPercent: 0,
-        opacity: 1,
-      });
-      gsap.set(hoverText[index], {
-        yPercent: 100,
-        opacity: 0,
-      });
+  const footerHover = (enable) => {
+    if (isHoverRegistered) return;
+    isHoverRegistered = true;
+    if (!enable) return;
+    hoverToggle.forEach((toggle, index) => {
+      toggle.addEventListener("mouseenter", () => {
+        gsap.set(defaultText[index], {
+          yPercent: 0,
+          opacity: 1,
+        });
+        gsap.set(hoverText[index], {
+          yPercent: 100,
+          opacity: 0,
+        });
 
-      gsap.to(defaultText[index], {
-        yPercent: -100,
-        duration: 0.5,
-        opacity: 0,
-        ease: "power2.In",
-      });
-      gsap.to(hoverText[index], {
-        yPercent: 0,
-        duration: 0.5,
-        opacity: 1,
-        ease: "power2.In",
+        gsap.to(defaultText[index], {
+          yPercent: -100,
+          duration: 0.5,
+          opacity: 0,
+          ease: "power2.In",
+        });
+        gsap.to(hoverText[index], {
+          yPercent: 0,
+          duration: 0.5,
+          opacity: 1,
+          ease: "power2.In",
+        });
       });
     });
+  };
+
+  footerHover(lg.matches);
+
+  lg.addEventListener("change", () => {
+    footerHover(lg.matches);
   });
 };
 
